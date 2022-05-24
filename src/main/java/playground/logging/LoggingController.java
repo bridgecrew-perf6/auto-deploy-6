b3e0b725de73e876945e941ac20c6033b9b6cff8
naneun <rmtlrksdp@naver.com>
2022-05-25 08:07:22 +0900

    update

--- src/main/java/playground/logging/LoggingController.java
+++ src/main/java/playground/logging/LoggingController.java
@@ -2,17 +2,15 @@ package playground.logging;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class LoggingController {
 
     @Value("${logging-module.version}")
     private String version;
 
-    @GetMapping
+    @GetMapping("/")
     public String version() {
         return String.format("Project Version : %s", version);
     }
